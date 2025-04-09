c8d01584441439592b8462468cb2dfee6daec6ea
Shawn Richardson <gshawnr@gmail.com>
2025-04-09 16:54:15 -0400

    TSC and FE updates

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -11,15 +11,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API runnning");
+app.get("/", (req: Request, res: Response) => {
+  res.send("API runnning with TS");
 });
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI as string)
   .then(() => {
     console.log("MongoDB connected");
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
